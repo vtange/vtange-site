1e3bbd20727afc755b286f62ee362abbc76ff0a1
Victor Tang <vtange@users.noreply.github.com>
2021-06-20 11:40:08 -0700

    quick chopping some bytes so we can rebuild

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 
 document.addEventListener("DOMContentLoaded", function () {
     var i = 0;
-    var list = document.getElementById("list");
+    var l = document.getElementById("list");
     function onBtnClick(bLeft, e) {
-        var w = list.firstElementChild.offsetWidth;
-        i = bLeft ? Math.max(0,i-1) : Math.min(list.children.length-2, i+1);
-        list.style.transform = "translateX(-"+w*i+"px)";
+        var w = l.firstElementChild.offsetWidth;
+        i = bLeft ? Math.max(0,i-1) : Math.min(l.children.length-2, i+1);
+        l.style.transform = "translateX(-"+w*i+"px)";
     }
     window.addEventListener('resize', function(e){
         if (window.innerWidth < 600) {
             i = 0;
-            list.style.transform = "none";
+            l.style.transform = "none";
         }
     });
     document.getElementById("left").onclick = onBtnClick.bind(null,true);
